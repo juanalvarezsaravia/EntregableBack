4a180fc2b2554974dedeb22575e8ddf8a80c79dc
Julian Sucerquia <andresucerquia@gmail.com>
2023-12-19 15:38:52 -0500

    Actualizar tipo de dato de results a Array

--- src/schemas/search.schema.ts
+++ src/schemas/search.schema.ts
@@ -3,10 +3,10 @@ import { Schema, model } from "mongoose";
 
 
 const searchSchema = new Schema({
-    username: { type: String, required: true, index: true },
-    results: { type: String, required: true, index: true },
-    searchTerm: { type: String, index: true },
-    timestamp: { type: String, index: true },
+  username: { type: String, required: true, index: true },
+  results: { type: Array, required: true, index: true },
+  searchTerm: { type: String, index: true },
+  timestamp: { type: String, index: true },
 
 })
 
